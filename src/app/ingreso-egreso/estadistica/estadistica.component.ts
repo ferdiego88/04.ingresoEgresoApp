a21729f065b9746f41937c736295461760952420
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-10-15 12:34:54 -0500

    nombre del Usuario

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -49,7 +49,11 @@ export class EstadisticaComponent implements OnInit {
   }
 
   generarEstadistica(items: IngresoEgreso[]){
-    console.log(items);
+    this.totalEgresos = 0;
+    this.totalIngresos = 0;
+    this.egresos = 0;
+    this.ingresos = 0;
+
     for (const item of items) {
       if (item.tipo === 'ingreso') {
         this.totalIngresos += item.monto;
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { AuthService } from '../../services/auth.service';
+import { AppState } from '../../app.reducer';
+import { Usuario } from 'src/app/models/usuario.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
 
+export class SidebarComponent implements OnInit, OnDestroy {
+
+  user!: Usuario
+  userSubs: Subscription = new Subscription
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private store: Store<AppState>) { }
+
+  ngOnDestroy(): void {
+    this.userSubs.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.userSubs = this.store.select('user').subscribe( ({user}) => {
+      this.user = user
+    })
   }
 
   logout() {
